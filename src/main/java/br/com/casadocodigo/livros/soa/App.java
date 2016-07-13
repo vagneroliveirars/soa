48633654dfb68d0df1a8eee41c486cc528756fe5
vagneroliveirars <vagner.oliveirars@gmail.com>
2016-07-13 15:20:28 -0300

    first commit

---
+++ src/main/java/br/com/casadocodigo/livros/soa/App.java
@@ -0,0 +1,13 @@
+package br.com.casadocodigo.livros.soa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
