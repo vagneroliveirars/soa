d30ebcc55f0a1604de1615a8ca2f37abbb8ec9a3
vagneroliveirars <vagner.oliveirars@gmail.com>
2016-07-20 20:18:20 -0300

    Added module soa-cap04-jetty.

---
+++ soa-cap04-jetty/src/main/java/com/knight/estoque/daos/LivroDAO.java
@@ -0,0 +1,54 @@
+package com.knight.estoque.daos;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import com.knight.estoque.modelos.Autor;
+import com.knight.estoque.modelos.EBook;
+import com.knight.estoque.modelos.Livro;
+
+public class LivroDAO {
+	
+	private static List<Livro> livros;
+	
+	static {
+		livros = new ArrayList<>();
+		livros.add(new Livro(2012, new ArrayList<>(Arrays.asList(new Autor(
+				"Paulo Silveira", new Date()), new Autor("Adriano Almeida",
+				new Date()))), "Casa do Código", "Guia do Programador",
+				"Vá do \"nunca programei\" ..."));
+
+		livros.add(new Livro(2012, new ArrayList<>(Arrays.asList(new Autor(
+				"Vinícius Baggio Fuentes", new Date()))), "Casa do Código",
+				"Ruby on Rails", "Crie rapidamente aplicações web"));
+		
+		EBook eBook = new EBook(2012, new ArrayList<Autor>(
+				Arrays.asList(new Autor("Alexandre Saudade", new Date()))), 
+				"Casa do Código", "SOA Aplicado", "Aprenda SOA de forma prática");
+		
+		livros.add(eBook);
+	}
+	
+	public List<Livro> listarLivros() {
+		return livros;
+	}
+	
+	public List<Livro> listarLivros(Integer numeroDaPagina, Integer tamanhoDaPagina) {
+		List<Livro> livros = listarLivros();
+
+		int indiceInicial = numeroDaPagina * tamanhoDaPagina;
+		int indiceFinal = indiceInicial + tamanhoDaPagina;
+
+		indiceFinal = indiceFinal > livros.size() ? livros.size() : indiceFinal;
+		indiceInicial = indiceInicial > indiceFinal ? indiceFinal : indiceInicial;
+
+		return livros.subList(indiceInicial, indiceFinal);
+	}
+	
+	public void criarLivro(Livro livro) {
+		livros.add(livro);
+	}
+
+}
