e126187a1d9f805294fbe16c9b6aeef58cfb92bc
vagneroliveirars <vagner.oliveirars@gmail.com>
2016-08-03 20:02:07 -0300

    Added method to create new users.

--- soa-cap05/src/main/java/com/knight/usuarios/servicos/UsuariosService.java
+++ soa-cap05/src/main/java/com/knight/usuarios/servicos/UsuariosService.java
@@ -1,16 +1,22 @@
 package com.knight.usuarios.servicos;
 
+import java.net.URI;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 import com.knight.usuarios.modelos.Usuario;
 import com.knight.usuarios.modelos.Usuarios;
@@ -39,6 +45,17 @@ public class UsuariosService {
 		}
 		
 		return Response.status(Status.NOT_FOUND).build();
+	}
+	
+	@POST
+	public Response create(@Context UriInfo uriInfo, Usuario usuario) {
+		this.entityManager.persist(usuario);
+		
+		// Build URL where the resource is available
+		UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
+		URI location = uriBuilder.path("/{id}").build(usuario.getId());
+		
+		return Response.created(location).build();
 	}
 	
 }
