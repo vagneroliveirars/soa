edf735bcfe34b03d46527a7e2536fd94d156eb9c
vagneroliveirars <vagner.oliveirars@gmail.com>
2016-08-06 20:32:32 -0300

    Added class Link and interface RESTEntity.

--- soa-cap05/src/main/java/com/knight/usuarios/modelos/Usuarios.java
+++ soa-cap05/src/main/java/com/knight/usuarios/modelos/Usuarios.java
@@ -1,5 +1,7 @@
 package com.knight.usuarios.modelos;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -7,19 +9,26 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.knight.usuarios.modelos.rest.Link;
+import com.knight.usuarios.modelos.rest.RESTEntity;
+
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
-public class Usuarios {
+public class Usuarios implements RESTEntity {
 	
 	@XmlElement(name = "usuario")
 	private Collection<Usuario> usuarios;
+	
+	@XmlElement(name = "link")
+	private Collection<Link> links;
 
 	public Usuarios() {
 
 	}
 
-	public Usuarios(Collection<Usuario> usuarios) {
+	public Usuarios(Collection<Usuario> usuarios, Link... link) {
 		this.usuarios = usuarios;
+		this.links = new ArrayList<Link>(Arrays.asList(link));
 	}
 
 	public Collection<Usuario> getUsuarios() {
@@ -28,6 +37,15 @@ public class Usuarios {
 
 	public void setUsuarios(Collection<Usuario> usuarios) {
 		this.usuarios = usuarios;
+	}
+	
+	@Override
+	public void adicionarLink(Link link) {
+		if (this.links == null) {
+			this.links = new ArrayList<Link>();
+		}
+		
+		this.links.add(link);
 	}
 	
 }
