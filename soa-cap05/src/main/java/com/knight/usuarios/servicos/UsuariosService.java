04f0e1e856e1de6f2c756628ebfe198e9ce8b92c
vagneroliveirars <vagner.oliveirars@gmail.com>
2016-08-08 18:28:36 -0300

    Added method criarLinksUsuarios.

--- soa-cap05/src/main/java/com/knight/usuarios/servicos/UsuariosService.java
+++ soa-cap05/src/main/java/com/knight/usuarios/servicos/UsuariosService.java
@@ -1,6 +1,8 @@
 package com.knight.usuarios.servicos;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
 import javax.ejb.Stateless;
@@ -8,6 +10,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
@@ -15,6 +18,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -25,6 +29,7 @@ import javax.ws.rs.core.UriInfo;
 import com.knight.usuarios.modelos.Imagem;
 import com.knight.usuarios.modelos.Usuario;
 import com.knight.usuarios.modelos.Usuarios;
+import com.knight.usuarios.modelos.rest.Link;
 
 @Stateless
 @Path("/usuarios")
@@ -35,8 +40,21 @@ public class UsuariosService {
 	@PersistenceContext
 	private EntityManager entityManager;
 	
+	private static final String CAMPO_DESCRICAO_IMAGEM = "Descricao";
+
+	private static final String PARAM_INICIO = "inicio";
+
+	private static final String PARAM_TAMANHO_PAGINA = "tamanhoPagina";
+	
 	@GET
-	public Usuarios listarUsuarios() {
+	public Usuarios listarUsuarios(@HeaderParam("If-Modified-Since") Date modifiedSince,
+			@QueryParam(PARAM_INICIO) @DefaultValue("0") Integer inicio,
+			@QueryParam(PARAM_TAMANHO_PAGINA) @DefaultValue("20")
+			Integer tamanhoPagina,
+			@Context UriInfo uriInfo) {
+		
+		
+		
 		return new Usuarios(this.entityManager.createQuery("select u from Usuario u", Usuario.class).getResultList());
 	}
 	
@@ -84,13 +102,13 @@ public class UsuariosService {
 			return Response.notModified().build();
 		}
 		
-		return Response.ok(imagem.getDados(), imagem.getTipo()).header("Descricao", imagem.getDescricao()).build();
+		return Response.ok(imagem.getDados(), imagem.getTipo()).header(CAMPO_DESCRICAO_IMAGEM, imagem.getDescricao()).build();
 	}
 	
 	@PUT
 	@Path("/{id}")
 	@Consumes("image/*")
-	public Response adicionarImagem(@HeaderParam("Descricao") String descricao,
+	public Response adicionarImagem(@HeaderParam(CAMPO_DESCRICAO_IMAGEM) String descricao,
 			@PathParam("id") Long idUsuario,
 			@Context HttpServletRequest httpServletRequest, 
 			byte[] dadosImagem) {
@@ -111,6 +129,65 @@ public class UsuariosService {
 		this.entityManager.merge(usuario);
 		
 		return Response.noContent().build();
+	}
+	
+	private Link[] criarLinksUsuarios(UriInfo uriInfo, Integer tamanhoPagina, Integer inicio, Long numeroUsuarios) {
+		Collection<Link> links = new ArrayList<Link>();
+		
+		double numeroUsuariosDouble = numeroUsuarios;
+		double tamanhoPaginaDouble = tamanhoPagina;
+		
+		// Arredondamento apra cima para fornecer o número certo de páginas
+		Long numeroPaginas = (long) Math.ceil(numeroUsuariosDouble / tamanhoPaginaDouble);
+		
+		// O resultado da divisão será um int
+		Long paginaAtual = new Long(inicio / tamanhoPagina);
+		
+		Link linkPrimeiraPagina = new Link(
+	            UriBuilder.fromPath(uriInfo.getPath()).queryParam(PARAM_INICIO, 0)
+	                  .queryParam(PARAM_TAMANHO_PAGINA, tamanhoPagina).build()
+	                  .toString(), "primeiraPagina");
+	
+		links.add(linkPrimeiraPagina);
+		
+		if (paginaAtual > 0) {
+			if (paginaAtual <= numeroPaginas) {
+				Link linkPaginaAnterior = new Link(UriBuilder
+						.fromPath(uriInfo.getPath())
+						.queryParam(PARAM_INICIO, (paginaAtual - 1) * tamanhoPagina)
+						.queryParam(PARAM_TAMANHO_PAGINA, tamanhoPagina)
+						.build().toString(), "paginaAnterior");
+
+				links.add(linkPaginaAnterior);
+			} else {
+				Link linkPaginaAnterior = new Link(UriBuilder
+						.fromPath(uriInfo.getPath())
+						.queryParam(PARAM_INICIO, (numeroPaginas - 1) * tamanhoPagina)
+						.queryParam(PARAM_TAMANHO_PAGINA, tamanhoPagina)
+						.build().toString(), "paginaAnterior");
+				
+				links.add(linkPaginaAnterior);
+			}
+		}
+		
+		if (paginaAtual < (numeroPaginas - 1)) {
+			Link linkProximaPagina = new Link(UriBuilder
+							.fromPath(uriInfo.getPath())
+							.queryParam(PARAM_INICIO, (paginaAtual + 1) * tamanhoPagina)
+							.queryParam(PARAM_TAMANHO_PAGINA, tamanhoPagina)
+							.build().toString(), "proximaPagina");
+			
+			links.add(linkProximaPagina);
+		}
+		
+		Link linkUltimaPagina = new Link(UriBuilder.fromPath(uriInfo.getPath())
+				.queryParam(PARAM_INICIO, (numeroPaginas - 1) * tamanhoPagina)
+				.queryParam(PARAM_TAMANHO_PAGINA, tamanhoPagina).build()
+				.toString(), "ultimaPagina");
+		
+		links.add(linkUltimaPagina);
+		
+		return links.toArray(new Link[] {});
 	}
 	
 }
