ffd580c30f9cf6006bc8f0a7a647aa8823ab11a3
vagneroliveirars <vagner.oliveirars@gmail.com>
2016-08-12 19:04:44 -0300

    Added new unit tests.

--- soa-cap05/src/test/java/com/knight/usuarios/servicos/testes/integracao/UsuariosServiceIT.java
+++ soa-cap05/src/test/java/com/knight/usuarios/servicos/testes/integracao/UsuariosServiceIT.java
@@ -5,14 +5,20 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.ws.rs.core.MediaType;
 
 import org.jboss.resteasy.client.ClientRequest;
 import org.jboss.resteasy.client.ClientResponse;
+import org.jboss.resteasy.client.ProxyFactory;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.knight.usuarios.modelos.Usuario;
+import com.knight.usuarios.modelos.Usuarios;
 import com.knight.usuarios.servicos.UsuariosServiceInterface;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 public class UsuariosServiceIT {
 	
@@ -28,6 +34,68 @@ public class UsuariosServiceIT {
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 		ImageIO.write(bufferedImage, "jpg", byteArrayOutputStream);
 		this.imagemTeste = byteArrayOutputStream.toByteArray();
+	}
+	
+	@Test
+	public void testeCriacaoUsuarios() throws Exception {
+		Usuario usuario = new Usuario();
+		usuario.setNome("Vagner");
+		usuario.setLogin("admin");
+		usuario.setSenha("123");
+		
+		XStream stream = new XStream(new DomDriver());
+		stream.alias("usuario", Usuario.class);
+		String xmlUsuario = stream.toXML(usuario);
+		
+		ClientResponse<?> clientResponse = new ClientRequest(USUARIOS_CONTEXT).body(MediaType.APPLICATION_XML, xmlUsuario).post();
+		
+		Assert.assertEquals(201, clientResponse.getStatus());
+	}
+	
+	@Test
+	public void testeRecepcaoUsuarios() {
+		UsuariosServiceInterface usuariosServiceInterface = ProxyFactory
+				.create(UsuariosServiceInterface.class, SERVICES_CONTEXT);
+		
+		@SuppressWarnings("unchecked")
+		ClientResponse<Usuarios> response = (ClientResponse<Usuarios>) usuariosServiceInterface.listarUsuarios(null, null, null, null);
+		
+		Assert.assertEquals(200, response.getStatus());
+		
+		Usuarios usuarios = response.getEntity(Usuarios.class);
+		
+		Assert.assertNotNull(usuarios);
+		Assert.assertNotNull(usuarios.getUsuarios());
+		Assert.assertEquals(1, usuarios.getUsuarios().size());
+	}
+	
+	@Test
+	public void testeRecepcaoUsuariosClientRequest() throws Exception {
+		ClientResponse<Usuario> response = new ClientRequest(USUARIOS_CONTEXT + "/{id}").pathParameters(1).accept(MediaType.APPLICATION_XML).get(Usuario.class);
+		
+		Usuario usuario = response.getEntity(Usuario.class);
+		
+		Assert.assertNotNull(usuario);
+	}
+	
+	@Test
+	public void testeCriacaoImagens() throws Exception {
+		ClientResponse<?> clientResponse = new ClientRequest(USUARIOS_CONTEXT
+				+ "/{id}")
+				.pathParameters(1)
+				.header(UsuariosServiceInterface.CAMPO_DESCRICAO_IMAGEM,
+						"skate").body("image/jpg", imagemTeste).put();
+		
+		Assert.assertEquals(204, clientResponse.getStatus());
+		
+		clientResponse = new ClientRequest(USUARIOS_CONTEXT + "/{id}").pathParameters(1).accept("image/*").get(byte[].class);
+		
+		Assert.assertEquals(200, clientResponse.getStatus());
+		Assert.assertArrayEquals(imagemTeste, (byte[]) clientResponse.getEntity());
+		
+		String descricaoImagem = clientResponse.getHeaders().getFirst(UsuariosServiceInterface.CAMPO_DESCRICAO_IMAGEM);
+		
+		Assert.assertEquals("skate", descricaoImagem);
 	}
 	
 	@Test
